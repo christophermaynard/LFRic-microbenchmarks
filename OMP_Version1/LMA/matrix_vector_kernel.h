18680e3a4a288fc6574f1973501b3770b5665cf5
Mark Gibson <mjgibson44@outlook.com>
2021-12-20 11:40:05 +0000

    Developed OpenMP version using function directive

---
+++ OMP_Version1/LMA/matrix_vector_kernel.h
@@ -0,0 +1,14 @@
+/* -----------------------------------------------------------------------------*/
+/* Copyright (c) 2017,  Met Office, on behalf of HMSO and Queen's Printer       */
+/* For further details please refer to the file LICENCE.original which you      */
+/* should have received as part of this distribution.                           */
+/*----------------------------------------------------------------------------- */
+#ifndef H_MATRIX_VECTOR_KERNEL
+#define H_MATRIX_VECTOR_KERNEL
+#include <stdlib.h>
+#include <stdio.h>
+
+void matrix_vector_code(int cell, int nlayers, double *lhs, double *x, int ncell_3d, double ***matrix, int ndf1, int undf, int **map1, int ndf2, int undf2, int **map2);
+
+void matrix_vector_code_1D(int cell, int nlayers, double* lh, double *x, int ncell_3d, double *matrix, int ndf1, int undf, int **map1, int ndf2, int undf2, int **map2);
+#endif
---
+++ OMP_Version1/common/dino_type.h
@@ -0,0 +1,35 @@
+/* -----------------------------------------------------------------------------
+ * Copyright (c) 2017,  Met Office, on behalf of HMSO and Queen's Printer       
+ * For further details please refer to the file LICENCE.original which you      
+ * should have received as part of this distribution.                           
+ *----------------------------------------------------------------------------- 
+ */
+
+#ifndef DINO_H
+#define DINO_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+
+#define MAX_FILE_LEN 128
+
+typedef struct{
+  FILE *fp;
+  char fn[MAX_FILE_LEN];
+  
+} dino_type;
+
+void dino_open(dino_type *dino);
+void dino_close(dino_type *dino);
+
+void input_scalar(dino_type *dino, int *scalar);
+void input_1d_int(dino_type *dino, int *array, int dim);
+void input_2d_int(dino_type *dino, int **array, int dim1, int dim2);
+
+void input_1d_double(dino_type *dino, double *array, int dim);
+void input_3d_double(dino_type *dino, double ***array, int dim1, int dim2, int dim3);
+
+void input_3d_double_1D(dino_type *dino, double *array, int dim1, int dim2, int dim3);
+#endif    
