c2515f4ee0f08fb99f47659b2ea32e2eb79d552b
Mark Gibson <mjgibson44@outlook.com>
2022-01-19 10:09:36 +0000

    reordered k-loop

--- C-version/LMA/matrix_vector_kernel.c
+++ C-version/LMA/matrix_vector_kernel.c
@@ -9,16 +9,17 @@
 void matrix_vector_code(int cell, int nlayers, double *lhs, double *x, int ncell_3d, double ***matrix, int ndf1, int undf, int **map1, int ndf2, int undf2, int **map2){
 
   int df1,df2,k,ik,kk;
-  for( k=0; k<nlayers; k++){
-    ik = cell*nlayers+k;
-    kk=k-1;
+  
     for( df1 = 0; df1<ndf1; df1++){
       for( df2 = 0; df2<ndf2; df2++){
-		
-	lhs[ map1[df1][cell] + kk ] +=  matrix[df1][df2][ik]*x[ map2[df2][cell] + kk];
-      }
+         for( k=0; k<nlayers; k++){
+            ik = cell*nlayers+k;
+            kk=k-1;
+	         lhs[ map1[df1][cell] + kk ] +=  matrix[df1][df2][ik]*x[ map2[df2][cell] + kk];
+      
+            }
+         }
     }
-  }
   
   return;
 }
